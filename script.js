5d098382881c63e7c94331491481235b94d57d3d
ashish <attiwari69@gmail.com>
2024-10-11 14:27:28 +0530

    modified in js file

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     const boxes = document.querySelectorAll('.box');
-  
-//     boxes.forEach(box => {
-//       box.addEventListener('click', () => {
-//         // Toggle the expanded state
-//         box.classList.toggle('expanded');
-//       });
-//     });
-//   });
-  
-
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     const boxes = document.querySelectorAll('.box');
-  
-//     boxes.forEach(box => {
-//       box.addEventListener('click', () => {
-//         // Collapse all boxes first
-//         boxes.forEach(b => b.classList.remove('expanded'));
-  
-//         // Expand the clicked box
-//         box.classList.add('expanded');
-//       });
-//     });
-//   });
-  
-
 document.addEventListener('DOMContentLoaded', () => {
     const boxes = document.querySelectorAll('.box');
   
